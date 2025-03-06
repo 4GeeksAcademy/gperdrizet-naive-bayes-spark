2f32b2c38b55060b94e419bc14381696afa61b3a
George Perdrizet <george@perdrizet.org>
2025-03-06 21:10:13 +0000

    Moved spark set-up script to devcontainer directory.

--- .devcontainer/get_spark.sh
+++ .devcontainer/get_spark.sh
@@ -2,4 +2,4 @@
 
 curl https://dlcdn.apache.org/spark/spark-3.5.5/spark-3.5.5-bin-hadoop3.tgz --output /tmp/spark.tgz
 mkdir $CODESPACE_VSCODE_FOLDER/spark
-tar -xf /tmp/spark.tgz -C $CODESPACE_VSCODE_FOLDER/spark
+tar -xf /tmp/spark.tgz -C $CODESPACE_VSCODE_FOLDER/spark --strip-components 1
