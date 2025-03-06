60a3037a9a643cac7d06257a81815c0c9c3ce641
George Perdrizet <george@perdrizet.org>
2025-03-06 21:00:42 +0000

    Switched to using environment variable to specify path to project root dir.

--- get_spark.sh
+++ get_spark.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 curl https://dlcdn.apache.org/spark/spark-3.5.5/spark-3.5.5-bin-hadoop3.tgz --output /tmp/spark.tgz
-mkdir /workspaces/codespace-spark-cluster-headnode/spark
-tar -xf /tmp/spark.tgz -C /workspaces/codespace-spark-cluster-headnode/spark
+mkdir $CODESPACE_VSCODE_FOLDER/spark
+tar -xf /tmp/spark.tgz -C $CODESPACE_VSCODE_FOLDER/spark
