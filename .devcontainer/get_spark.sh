c219d4c90cd8de70db630c761acd640f2ede9842
George Perdrizet <george@perdrizet.org>
2025-03-07 01:51:46 +0000

    Added command to start spark.

--- .devcontainer/get_spark.sh
+++ .devcontainer/get_spark.sh
@@ -5,3 +5,4 @@ sudo apt-get install -y default-jdk
 curl https://dlcdn.apache.org/spark/spark-3.5.5/spark-3.5.5-bin-hadoop3.tgz --output /tmp/spark.tgz
 mkdir spark
 tar -xf /tmp/spark.tgz -C spark --strip-components 1
+spark/sbin/start-master.sh
