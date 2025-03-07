8031141c0f1da1d7c8698e8390aed8f4729b56ee
George Perdrizet <george@perdrizet.org>
2025-03-07 02:41:08 +0000

    Moved spark install to onCreateCommand in devcontainer configuration.

--- .devcontainer/get_spark.sh
+++ .devcontainer/get_spark.sh
@@ -3,6 +3,5 @@ sudo apt-get update
 sudo apt -y upgrade
 sudo apt-get install -y default-jdk
 curl https://dlcdn.apache.org/spark/spark-3.5.5/spark-3.5.5-bin-hadoop3.tgz --output /tmp/spark.tgz
-mkdir spark
-tar -xf /tmp/spark.tgz -C spark --strip-components 1
-spark/sbin/start-master.sh
+sudo mkdir /opt/spark
+tar -xf /tmp/spark.tgz -C /opt/spark --strip-components 1
