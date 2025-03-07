3a4e720de6d39e5061c2c81880f3258ac7fcf38f
George Perdrizet <george@perdrizet.org>
2025-03-07 00:37:07 +0000

    Added java install.

--- .devcontainer/get_spark.sh
+++ .devcontainer/get_spark.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk
 curl https://dlcdn.apache.org/spark/spark-3.5.5/spark-3.5.5-bin-hadoop3.tgz --output /tmp/spark.tgz
-mkdir $CODESPACE_VSCODE_FOLDER/spark
-tar -xf /tmp/spark.tgz -C $CODESPACE_VSCODE_FOLDER/spark --strip-components 1
+mkdir spark
+tar -xf /tmp/spark.tgz -C spark --strip-components 1
