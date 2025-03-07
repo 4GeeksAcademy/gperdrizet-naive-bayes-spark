3393d0287ca355f31adcff7beaa323c7037db7a7
George Perdrizet <george@perdrizet.org>
2025-03-07 03:23:50 +0000

    Updated spark install script.

--- .devcontainer/get_spark.sh
+++ .devcontainer/get_spark.sh
@@ -4,4 +4,4 @@ sudo apt -y upgrade
 sudo apt-get install -y default-jdk
 curl https://dlcdn.apache.org/spark/spark-3.5.5/spark-3.5.5-bin-hadoop3.tgz --output /tmp/spark.tgz
 sudo mkdir /opt/spark
-tar -xf /tmp/spark.tgz -C /opt/spark --strip-components 1
+sudo tar -xf /tmp/spark.tgz -C /opt/spark --strip-components 1
