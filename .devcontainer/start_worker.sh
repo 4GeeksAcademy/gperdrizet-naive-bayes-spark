95fd5a83cced19b0b1dc40f40f298f38e8a9dd35
George Perdrizet <george@perdrizet.org>
2025-03-11 16:48:16 +0000

    Added creation of ssh directory in user home.

--- .devcontainer/start_worker.sh
+++ .devcontainer/start_worker.sh
@@ -6,6 +6,7 @@
 # and opens SSH tunnel to hdeadnode on port 7077. Then starts Spark worker
 # process
 
+mkdir -p ~/.ssh
 git pull
 gh codespace ssh -c `cat .devcontainer/headnode_hostname` --config > ~/.ssh/config
 "ssh -vfN -L 7077:localhost:7077 cs.`cat .devcontainer/headnode_hostname`.main
