cf8723e35098a661742c6231373db92fd4e38f54
George Perdrizet <george@perdrizet.org>
2025-03-10 23:10:16 -0400

    Update setup_worker.sh

--- .devcontainer/worker/setup_worker.sh
+++ .devcontainer/worker/setup_worker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo apt intstall screen
 
 (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
 	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
@@ -7,8 +6,9 @@ sudo apt intstall screen
         && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
 	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
 	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-	&& sudo apt update \
+	&& sudo apt update -y \
 	&& sudo apt install gh -y
 
 mkdir ~/.ssh
 gh codespace ssh -c fuzzy-space-giggle-55g65wwg6xh4w45 --config > ~/.ssh/config
+ssh -vfN -L 7077:localhost:7077 cs.fuzzy-space-giggle-55g65wwg6xh4w45.main
